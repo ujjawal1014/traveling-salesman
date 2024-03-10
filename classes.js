cc2807ee76a664848f4b995953797f98507db9e9
Adiiii77 <123948618+Adiiii77@users.noreply.github.com>
2024-03-10 20:59:01 +0530

    Update classes.js

--- classes.js
+++ classes.js
@@ -6,14 +6,3 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-const sidebarButton = document.getElementById("sidebarButton");
-const buttonContainer = document.getElementById("buttonContainer");
-const closeButton = document.getElementById("closeButton");
-
-sidebarButton.addEventListener("click", () => {
-  buttonContainer.style.display = "none"; // Hide the buttons
-});
-
-closeButton.addEventListener("click", () => {
-  buttonContainer.style.display = "block"; // Show the buttons
-});
